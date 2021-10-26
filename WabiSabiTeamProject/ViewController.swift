338a785941bbd4244290fb3fb136432ff65e2a09
Edwin <osproject78@gmail.com>
2021-10-26 14:33:44 +0700

    Create Initial Project

---
+++ WabiSabiTeamProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WabiSabiTeamProject
+//
+//  Created by Edwin Niwarlangga on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
