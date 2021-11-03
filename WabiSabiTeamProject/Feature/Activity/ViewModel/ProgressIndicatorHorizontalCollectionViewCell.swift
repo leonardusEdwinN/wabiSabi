bdf946b0a9f941ce3be69b9a9d6eae8b26ea6380
zynhrus <albertonly898@gmail.com>
2021-11-03 14:48:45 +0700

    Create horizontal collection view

---
+++ WabiSabiTeamProject/Feature/Activity/ViewModel/ProgressIndicatorHorizontalCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressIndicatorHorizontalCollectionViewCell.swift
+//  WabiSabiTeamProject
+//
+//  Created by Albert . on 01/11/21.
+//
+
+import UIKit
+
+class ProgressIndicatorHorizontalCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var percentage: UILabel!
+    @IBOutlet weak var status: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
