e829e1eae608fe27eb9c01bddc933deaa17e81de
Edwin <osproject78@gmail.com>
2021-11-10 18:15:12 +0700

    Add Dummy Profile

---
+++ WabiSabiTeamProject/Feature/Profile/View/ProductTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductTableViewCell.swift
+//  WabiSabiTeamProject
+//
+//  Created by Edwin Niwarlangga on 10/11/21.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WabiSabiTeamProject/Feature/Profile/View/RoutineTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RoutineTableViewCell.swift
+//  WabiSabiTeamProject
+//
+//  Created by Edwin Niwarlangga on 10/11/21.
+//
+
+import UIKit
+
+class RoutineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
