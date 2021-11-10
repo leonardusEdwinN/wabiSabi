71742e4270837cf6c845c762a960f37faa6f53c9
Edwin <osproject78@gmail.com>
2021-11-10 16:24:08 +0700

    Add Font and Profile page

---
+++ WabiSabiTeamProject/Feature/Profile/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  WabiSabiTeamProject
+//
+//  Created by Edwin Niwarlangga on 10/11/21.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
