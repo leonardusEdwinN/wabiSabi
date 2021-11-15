2657b8a19d99425451c95fb65e6c07060f68ee90
Giovanni Tjahyamulia <giovannitjahyamulia@icloud.com>
2021-11-15 18:03:19 +0700

    ✅ Filter Products based on Level ✅ CoreData and CloudKit Model Data ✅ CoreData and CloudKit Configuration (Need to Be Checked ✅ CoreData and CloudKit function to CRUD

--- WabiSabiTeamProject/AppDelegate.swift
+++ WabiSabiTeamProject/AppDelegate.swift
@@ -10,9 +10,6 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -32,32 +29,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-    // MARK: - Core Data stack
+    // MARK: - Core Data Stack and Cloud Kit Container
 
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "WabiSabiTeamProject")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
+        let container = NSPersistentCloudKitContainer(name: "WabiSabiTeamProject")
+        
+        // Create a store description for a local store
+        let localStoreLocation = URL(fileURLWithPath: "/path/to/local.store")
+        let localStoreDescription =
+            NSPersistentStoreDescription(url: localStoreLocation)
+        localStoreDescription.configuration = "Local"
+        
+        // Create a store description for a CloudKit-backed local store
+        let cloudStoreLocation = URL(fileURLWithPath: "/path/to/cloud.store")
+        let cloudStoreDescription =
+            NSPersistentStoreDescription(url: cloudStoreLocation)
+        cloudStoreDescription.configuration = "Cloud"
+
+        // Set the container options on the cloud store
+        cloudStoreDescription.cloudKitContainerOptions =
+            NSPersistentCloudKitContainerOptions(
+                containerIdentifier: "com.my.container")
+        
+        // Update the container's list of store descriptions
+        container.persistentStoreDescriptions = [
+            cloudStoreDescription,
+            localStoreDescription
+        ]
+        
+        // Load both stores
+        container.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError("Could not load persistent stores. \(error!)")
             }
-        })
+        }
+        
         return container
     }()
 
@@ -76,6 +82,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
 
--- WabiSabiTeamProject/Feature/OnBoarding/ViewModel/SkinCareGuideViewController.swift
+++ WabiSabiTeamProject/Feature/OnBoarding/ViewModel/SkinCareGuideViewController.swift
@@ -12,66 +12,10 @@ class SkinCareGuideViewController: UIViewController {
     @IBOutlet weak var skincareRoutineTitle: UILabel!
     @IBOutlet weak var skincareProductTableView: UITableView!
     
-    let skinTypeRoutineProduct: [SkinTypeRoutine] = [
-        SkinTypeRoutine(icon: "🌞", name: "Morning Skin Care", skinType: [
-            SkinTypeProduct(name: "Oily", products: [
-                SkinCareProduct(icon: "", name: "Cleanser", description: "Gel/Foam Cleanser that contain salicylic/glycolic acid"),
-                SkinCareProduct(icon: "", name: "Toner", description: "Avoid alcohol based toner"),
-                SkinCareProduct(icon: "", name: "Serum", description: "Antioxidant Serum"),
-                SkinCareProduct(icon: "", name: "Sunscreen", description: "Zinc Oxide")
-            ]),
-            SkinTypeProduct(name: "Dry", products: [
-                SkinCareProduct(icon: "", name: "Cleanser", description: "Micellar water/cream Cleanser with humectants such as hyaluronic acid and glycerin"),
-                SkinCareProduct(icon: "", name: "Toner", description: "Avoid alcohol based toner"),
-                SkinCareProduct(icon: "", name: "Serum", description: "Antioxidant Serum"),
-                SkinCareProduct(icon: "", name: "Sunscreen", description: "Moisturizer with SPF")
-            ]),
-            SkinTypeProduct(name: "Combination", products: [
-                SkinCareProduct(icon: "", name: "Cleanser", description: "Gel/Foam Cleanser"),
-                SkinCareProduct(icon: "", name: "Toner", description: "Avoid alcohol based toner"),
-                SkinCareProduct(icon: "", name: "Moisturizer", description: "Lightweight"),
-                SkinCareProduct(icon: "", name: "Sunscreen", description: "Zinc Oxide")
-            ]),
-            SkinTypeProduct(name: "Normal", products: [
-                SkinCareProduct(icon: "", name: "Cleanser", description: "Micellar Water, Gel, Oil, or Cream Cleanser with Hyaluronic Acid to keep the complexion youthful and glowing"),
-                SkinCareProduct(icon: "", name: "Toner", description: "Avoid alcohol based toner"),
-                SkinCareProduct(icon: "", name: "Moisturizer", description: "Moisturizer with SPF")
-            ])
-        ]),
-        SkinTypeRoutine(icon: "🌓", name: "Night Skin Care", skinType: [
-            SkinTypeProduct(name: "Oily", products: [
-                SkinCareProduct(icon: "", name: "Cleanser", description: "Gel/Foam Cleanser that contain salicylic/glycolic acid"),
-                SkinCareProduct(icon: "", name: "Toner", description: "Avoid alcohol based toner"),
-                SkinCareProduct(icon: "", name: "Serum", description: "AHA/BHA, Retinol"),
-                SkinCareProduct(icon: "", name: "Moisturizer", description: "Oil-Free Water-Based"),
-                SkinCareProduct(icon: "", name: "Extras", description: "Clay Mask, Face Oil")
-            ]),
-            SkinTypeProduct(name: "Dry", products: [
-                SkinCareProduct(icon: "", name: "Cleanser", description: "Micellar water/cream Cleanser with humectants such as hyaluronic acid and glycerin"),
-                SkinCareProduct(icon: "", name: "Toner", description: "Avoid alcohol based toner"),
-                SkinCareProduct(icon: "", name: "Serum", description: "Retinol Serum"),
-                SkinCareProduct(icon: "", name: "Moisturizer", description: "Hydrating Moisturizer"),
-                SkinCareProduct(icon: "", name: "Extras", description: "At-Home Peel, Face Oil")
-            ]),
-            SkinTypeProduct(name: "Combination", products: [
-                SkinCareProduct(icon: "", name: "Cleanser", description: "Gel/Foam Cleanser"),
-                SkinCareProduct(icon: "", name: "Toner", description: "Avoid alcohol based toner"),
-                SkinCareProduct(icon: "", name: "Serum", description: "AHA/BHA, Retinol"),
-                SkinCareProduct(icon: "", name: "Moisturizer", description: "Lightweight"),
-                SkinCareProduct(icon: "", name: "Extras", description: "Clay Mask, Face Oil")
-            ]),
-            SkinTypeProduct(name: "Normal", products: [
-                SkinCareProduct(icon: "", name: "Cleanser", description: "Micellar Water, Gel, Oil, or Cream Cleanser with Hyaluronic Acid to keep the complexion youthful and glowing"),
-                SkinCareProduct(icon: "", name: "Toner", description: "Avoid alcohol based toner"),
-                SkinCareProduct(icon: "", name: "Serum", description: "Antioxidant"),
-                SkinCareProduct(icon: "", name: "Moisturizer", description: "Moisturizer"),
-                SkinCareProduct(icon: "", name: "Extras", description: "Glycolic Acid Serum")
-            ])
-        ])
-    ]
-    
-    var indexSelected: Int = 0;
     var skinTypeIndex: Int = 0;
+    var levelIndex: Int = 0;
+    
+    var skinTypeRoutine: SkinRoutineProduct!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -80,8 +24,9 @@ class SkinCareGuideViewController: UIViewController {
         skincareProductTableView.dataSource = self
         
         skinTypeIndex = UserDefaults.standard.integer(forKey: "skinTypes")
+        levelIndex = UserDefaults.standard.integer(forKey: "levelIndex")
         
-        skincareRoutineTitle.text = skinTypeRoutineProduct[indexSelected].name
+        skincareRoutineTitle.text = skinTypeRoutine.name
     }
 
     
@@ -92,21 +37,23 @@ class SkinCareGuideViewController: UIViewController {
 
 extension SkinCareGuideViewController : UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return skinTypeRoutineProduct[indexSelected].skinType[skinTypeIndex].products.count
+        return skinTypeRoutine.products.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = skincareProductTableView.dequeueReusableCell(withIdentifier: "skincareproductscell") as! SkinCareGuideTableViewCell
         
-        // cell.skinCareProductIcon.image = UIImage(named: dataSources[indexSelected].detailDatas[indexPath.row].dataIcon)
-        cell.skinCareProductName.text = skinTypeRoutineProduct[indexSelected].skinType[skinTypeIndex].products[indexPath.row].name
-        cell.skinCareProductDescription.text = skinTypeRoutineProduct[indexSelected].skinType[skinTypeIndex].products[indexPath.row].description
+        let data = skinTypeRoutine.products[indexPath.row]
+        
+        // cell.skinCareProductIcon.image = UIImage(named: data.icon)
+        cell.skinCareProductName.text = data.name
+        cell.skinCareProductDescription.text = data.description
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 75 + CGFloat((skinTypeRoutineProduct[indexSelected].skinType[skinTypeIndex].products[indexPath.row].description.count) / 28) * 18;
+        return 75 + CGFloat((skinTypeRoutine.products[indexPath.row].description.count) / 28) * 18;
     }
 }
