cdef163ba06eb764ce2e25097a488453917c3aa4
Edwin <osproject78@gmail.com>
2021-11-15 18:02:01 +0700

    Add UI Product

---
+++ WabiSabiTeamProject/Feature/AddProduct/AddProductViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddProductViewController.swift
+//  WabiSabiTeamProject
+//
+//  Created by Edwin Niwarlangga on 15/11/21.
+//
+
+import Foundation
+import UIKit
+
+class AddProductViewController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
